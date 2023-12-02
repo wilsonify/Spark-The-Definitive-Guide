5708c271598aa5170232b798fe2060cfed646151
wilsonify <tom.andrew.wilson@gmail.com>
2023-12-02 17:15:04 -0500

    clean up c03

--- code/python/Spark-Definitive-Guide/spark_definitive_guide/__init__.py
+++ code/python/Spark-Definitive-Guide/spark_definitive_guide/__init__.py
@@ -1,5 +1,4 @@
-import os.path
 from os.path import dirname, abspath
 
 path_to_here = abspath(dirname(__file__))
-path_to_data = abspath(f"{path_to_here}/../../data")
+path_to_data = abspath(f"{path_to_here}/../../../../data")
--- code/python/Spark-Definitive-Guide/spark_definitive_guide/a_gentle_introduction_to_spark_chapter_3_a_tour_of_sparks_toolset.py
+++ code/python/Spark-Definitive-Guide/spark_definitive_guide/a_gentle_introduction_to_spark_chapter_3_a_tour_of_sparks_toolset.py
@@ -1,57 +1,56 @@
-staticDataFrame = spark.read.format("csv")\
-  .option("header", "true")\
-  .option("inferSchema", "true")\
-  .load("/data/retail-data/by-day/*.csv")
+from pyspark.shell import spark
+from pyspark.sql.functions import date_format, col
+from pyspark.sql.functions import window, desc
+from pyspark.ml.feature import StringIndexer
+from pyspark.ml.feature import OneHotEncoder
+from pyspark.ml import Pipeline
+from pyspark.ml.clustering import KMeans
+from pyspark.sql import Row
+from pyspark.ml.feature import VectorAssembler
+
+from spark_definitive_guide import path_to_data
+
+staticDataFrame = spark.read.format("csv") \
+    .option("header", "true") \
+    .option("inferSchema", "true") \
+    .load(f"{path_to_data}/retail-data/by-day/*.csv")
 
 staticDataFrame.createOrReplaceTempView("retail_data")
 staticSchema = staticDataFrame.schema
 
+# COMMAND ----------
+
+
+staticDataFrame \
+    .selectExpr("CustomerId", "(UnitPrice * Quantity) as total_cost", "InvoiceDate") \
+    .groupBy(col("CustomerId"), window(col("InvoiceDate"), "1 day")) \
+    .sum("total_cost") \
+    .sort(desc("sum(total_cost)")) \
+    .show(5)
 
 # COMMAND ----------
 
-from pyspark.sql.functions import window, column, desc, col
-staticDataFrame\
-  .selectExpr(
-    "CustomerId",
-    "(UnitPrice * Quantity) as total_cost",
-    "InvoiceDate")\
-  .groupBy(
-    col("CustomerId"), window(col("InvoiceDate"), "1 day"))\
-  .sum("total_cost")\
-  .sort(desc("sum(total_cost)"))\
-  .show(5)
-
+streamingDataFrame = spark.readStream \
+    .schema(staticSchema) \
+    .option("maxFilesPerTrigger", 1) \
+    .format("csv") \
+    .option("header", "true") \
+    .load(f"{path_to_data}/retail-data/by-day/*.csv")
 
 # COMMAND ----------
 
-streamingDataFrame = spark.readStream\
-    .schema(staticSchema)\
-    .option("maxFilesPerTrigger", 1)\
-    .format("csv")\
-    .option("header", "true")\
-    .load("/data/retail-data/by-day/*.csv")
-
+purchaseByCustomerPerHour = streamingDataFrame \
+    .selectExpr("CustomerId", "(UnitPrice * Quantity) as total_cost", "InvoiceDate") \
+    .groupBy(col("CustomerId"), window(col("InvoiceDate"), "1 day")) \
+    .sum("total_cost")
 
 # COMMAND ----------
 
-purchaseByCustomerPerHour = streamingDataFrame\
-  .selectExpr(
-    "CustomerId",
-    "(UnitPrice * Quantity) as total_cost",
-    "InvoiceDate")\
-  .groupBy(
-    col("CustomerId"), window(col("InvoiceDate"), "1 day"))\
-  .sum("total_cost")
-
-
-# COMMAND ----------
-
-purchaseByCustomerPerHour.writeStream\
-    .format("memory")\
-    .queryName("customer_purchases")\
-    .outputMode("complete")\
+purchaseByCustomerPerHour.writeStream \
+    .format("memory") \
+    .queryName("customer_purchases") \
+    .outputMode("complete") \
     .start()
-
 
 # COMMAND ----------
 
@@ -59,94 +58,77 @@ spark.sql("""
   SELECT *
   FROM customer_purchases
   ORDER BY `sum(total_cost)` DESC
-  """)\
-  .show(5)
-
-
-# COMMAND ----------
-
-from pyspark.sql.functions import date_format, col
-preppedDataFrame = staticDataFrame\
-  .na.fill(0)\
-  .withColumn("day_of_week", date_format(col("InvoiceDate"), "EEEE"))\
-  .coalesce(5)
-
+  """) \
+    .show(5)
 
 # COMMAND ----------
 
-trainDataFrame = preppedDataFrame\
-  .where("InvoiceDate < '2011-07-01'")
-testDataFrame = preppedDataFrame\
-  .where("InvoiceDate >= '2011-07-01'")
 
-
-# COMMAND ----------
-
-from pyspark.ml.feature import StringIndexer
-indexer = StringIndexer()\
-  .setInputCol("day_of_week")\
-  .setOutputCol("day_of_week_index")
-
+preppedDataFrame = staticDataFrame \
+    .na.fill(0) \
+    .withColumn("day_of_week", date_format(col("InvoiceDate"), "EEEE")) \
+    .coalesce(5)
 
 # COMMAND ----------
 
-from pyspark.ml.feature import OneHotEncoder
-encoder = OneHotEncoder()\
-  .setInputCol("day_of_week_index")\
-  .setOutputCol("day_of_week_encoded")
-
-
-# COMMAND ----------
-
-from pyspark.ml.feature import VectorAssembler
-
-vectorAssembler = VectorAssembler()\
-  .setInputCols(["UnitPrice", "Quantity", "day_of_week_encoded"])\
-  .setOutputCol("features")
-
+trainDataFrame = preppedDataFrame \
+    .where("InvoiceDate < '2011-07-01'")
+testDataFrame = preppedDataFrame \
+    .where("InvoiceDate >= '2011-07-01'")
 
 # COMMAND ----------
 
-from pyspark.ml import Pipeline
 
-transformationPipeline = Pipeline()\
-  .setStages([indexer, encoder, vectorAssembler])
+indexer = StringIndexer() \
+    .setInputCol("day_of_week") \
+    .setOutputCol("day_of_week_index")
 
+# COMMAND ----------
+
+
+encoder = OneHotEncoder() \
+    .setInputCol("day_of_week_index") \
+    .setOutputCol("day_of_week_encoded")
+
+# COMMAND ----------
+
+
+vectorAssembler = VectorAssembler() \
+    .setInputCols(["UnitPrice", "Quantity", "day_of_week_encoded"]) \
+    .setOutputCol("features")
+
+# COMMAND ----------
+
+
+transformationPipeline = Pipeline() \
+    .setStages([indexer, encoder, vectorAssembler])
 
 # COMMAND ----------
 
 fittedPipeline = transformationPipeline.fit(trainDataFrame)
 
-
 # COMMAND ----------
 
 transformedTraining = fittedPipeline.transform(trainDataFrame)
 
-
 # COMMAND ----------
 
-from pyspark.ml.clustering import KMeans
-kmeans = KMeans()\
-  .setK(20)\
-  .setSeed(1L)
 
+kmeans = KMeans() \
+    .setK(20) \
+    .setSeed(1)
 
 # COMMAND ----------
 
 kmModel = kmeans.fit(transformedTraining)
 
-
 # COMMAND ----------
 
 transformedTest = fittedPipeline.transform(testDataFrame)
 
-
 # COMMAND ----------
 
-from pyspark.sql import Row
 
-spark.sparkContext.parallelize([Row(1), Row(2), Row(3)]).toDF()
-
+spark.sparkContext.parallelize([Row("1"), Row("2"), Row("3")]).toDF()
 
 # COMMAND ----------
-
